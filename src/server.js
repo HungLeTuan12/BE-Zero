98c3f48cdcf5827ed58a95c3d4001d8e3036825a
Hung Le <hunglehung812@gmail.com>
2023-06-28 16:23:35 +0700

    video 6

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path');
 const configViewEngine = require('./config/viewEngine');
 const webRoute = require('./routes/web');
-const app = express()
+const connection = require('./config/database');
+const app = express();
 const port = process.env.PORT || 8888;
 const hostName = process.env.HOST_NAME;
 // console.log(">>> process env", process.env);
@@ -13,6 +14,18 @@ configViewEngine(app);
 
 // khai bao route
 app.use(webRoute);
+
+// Test connection to database
+
+
+connection.query(
+   'SELECT * FROM Users u',
+   function(err, results, fileds) {
+      console.log('>>>result= ', results);
+      // console.log('>>>fields= ', fileds);
+
+   }
+)
 
 app.listen(port,hostName, () => {
   console.log(`Example app listening on port ${port}`)
